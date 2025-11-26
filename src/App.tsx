17f2c40467b6c21766fb1f268cf3e76c9ef88b5e
rodrigoronin <liramailbox@gmail.com>
2025-11-26 13:07:10 -0300

    feat: alignment changes
    changing parent <div> to <main> and adding a class to centralize it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header/Header";
+import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Header />
+    </>
+  );
 }
 
 export default App;
