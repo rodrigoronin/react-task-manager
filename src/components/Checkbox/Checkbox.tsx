22490c1c9b9e7104740d451ed7f47ae4be6e1f28
rodrigoronin <liramailbox@gmail.com>
2025-11-26 17:03:29 -0300

    feat: added the prop completed to the Checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import style from "./Checkbox.module.css";
 
-const Checkbox = () => {
-  return <span className={style.checkbox}></span>;
+const Checkbox = ({ completed = false }: { completed?: boolean }) => {
+  return <span className={`${style.checkbox} ${completed && style.completed}`}></span>;
 };
 
 export default Checkbox;
