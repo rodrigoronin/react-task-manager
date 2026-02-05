d32d35f489f11e19d54bf1d9a0e1f793c8690d05
rodrigoronin <liramailbox@gmail.com>
2026-02-05 14:59:20 -0300

    feat: login page
    added a simple login page with styles (no behaviour yet)

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,52 @@
+import { useState, type FormEvent } from "react";
+import Card from "../../components/Card/Card";
+import Input from "../../components/Input/Input";
+
+import style from "./LoginPage.module.css";
+
+interface IFormData {
+  email: string;
+  password: string;
+}
+
+const LoginPage = () => {
+  const [formData, setFormaData] = useState<IFormData>({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    console.log("login in...");
+  }
+
+  return (
+    <Card>
+      <div className={style["login-container"]}>
+        <h1 className={style["login-container-title"]}>Login</h1>
+
+        <form className={style["login-container-form"]} action="submit" onSubmit={handleSubmit}>
+          <Input
+            value={formData.email}
+            placeholderText="e-mail"
+            onChange={(e) => setFormaData((prev) => ({ ...prev, email: e.target.value }))}
+          />
+
+          <Input
+            type="password"
+            placeholderText="password"
+            value={formData.password}
+            onChange={(e) => setFormaData((prev) => ({ ...prev, password: e.target.value }))}
+          />
+
+          <button className={style["login-container-form-button"]} type="submit">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Card>
+  );
+};
+
+export default LoginPage;
